8cc052fc0fc78e3344f7b8efb7d2e616068ea2a5
benbright065@gmail.com <benbright065@gmail>
2021-09-26 05:04:06 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/crud/waybill/sender/waybill_sender/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.crud.waybill.sender.waybill_sender
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/waybill_sender/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.waybill_sender
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
